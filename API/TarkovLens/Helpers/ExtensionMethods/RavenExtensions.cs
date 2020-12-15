17464e90cea5d4b3ec1cbcdd38144349a3be3886
Chris Llewellyn <chr.llewellyn@gmail.com>
2020-12-15 13:06:06 +0000

    Extension methods

---
+++ API/TarkovLens/Helpers/ExtensionMethods/RavenExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TarkovLens.Helpers.ExtensionMethods
+{
+    public static class RavenExtensions
+    {
+        public static string AddCollectionName(this string id, string collectionName)
+        {
+            if (id.IsNullOrEmpty())
+            {
+                return null;
+            }
+            else if (id.ToLower().StartsWith(collectionName.ToLower()))
+            {
+                return id;
+            }
+            return string.Format("{0}/{1}", collectionName, id);
+        }
+
+        public static List<string> AddCollectionName(this IEnumerable<string> list, string collectionName)
+        {
+            var returnList = new List<string>();
+            foreach (var item in list)
+            {
+                returnList.Add(item.AddCollectionName(collectionName));
+            }
+
+            return returnList;
+        }
+
+        public static string StripCollectionName(this string id)
+        {
+            if (id.IsNullOrEmpty()) return string.Empty;
+            if (!id.Contains("/"))
+            {
+                return id;
+            }
+            return id.Substring(id.IndexOf("/") + 1);
+        }
+
+        public static List<string> StripCollectionName(this IEnumerable<string> list)
+        {
+            var returnList = new List<string>();
+            foreach (var item in list)
+            {
+                returnList.Add(item.StripCollectionName());
+            }
+
+            return returnList;
+        }
+    }
+}
---
+++ API/TarkovLens/Helpers/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TarkovLens.Helpers.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsEmail(this string email)
+        {
+            string pattern =
+                @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+            pattern =
+                @"^([a-zA-Z0-9_\-\.]+)([\+a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+            if (email != null) return Regex.IsMatch(email, pattern);
+            else return false;
+        }
+    }
+}
