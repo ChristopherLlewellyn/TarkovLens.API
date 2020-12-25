2b35dc8fbb77b02e1a87218734ee7c6df78b169a
Chris Llewellyn <chr.llewellyn@gmail.com>
2020-12-25 14:47:18 +0000

    Env updates

--- API/TarkovLens/Startup.cs
+++ API/TarkovLens/Startup.cs
@@ -21,9 +21,15 @@ namespace TarkovLens
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
