08c242cdbae3d4dad63cb7965decf2fa98c6bb64
ChristopherLlewellyn <chr.llewellyn@gmail.com>
2021-04-13 11:42:51 +0100

    Added GQL client and injected Tarkov-Tools service

---
+++ API/TarkovLens/Services/TarkovTools/TarkovToolsItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TarkovLens.Services.TarkovTools
+{
+    public class TarkovToolsItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("iconLink")]
+        public string IconLink { get; set; }
+
+        [JsonProperty("imageLink")]
+        public string ImageLink { get; set; }
+
+        [JsonProperty("avg24hPrice")]
+        public int Avg24hPrice { get; set; }
+    }
+}
---
+++ API/TarkovLens/Services/TarkovTools/TarkovToolsService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Abstractions;
+
+namespace TarkovLens.Services.TarkovTools
+{
+    public interface ITarkovToolsService
+    {
+        public Task GetItemsByKindAsync();
+    }
+
+    public class TarkovToolsService : ITarkovToolsService
+    {
+        private IGraphQLClient _tarkovToolsClient;
+
+        public TarkovToolsService(IGraphQLClient tarkovToolsClient)
+        {
+            _tarkovToolsClient = tarkovToolsClient;
+        }
+
+        public async Task GetItemsByKindAsync()
+        {
+            var request = new GraphQLRequest
+            {
+                Query = @"
+                {
+                    itemsByType(type: ammo)
+                    {
+                        id,
+                        name,
+                        iconLink,
+                        imageLink,
+                        avg24hPrice
+                    }
+                }"
+            };
+
+            var response = await _tarkovToolsClient.SendQueryAsync<TarkovToolsItem>(request);
+            throw new NotImplementedException();
+        }
+    }
+}
